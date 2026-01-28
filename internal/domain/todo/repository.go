7a33c1bd29ba3e4f546eb72ca9c87a54707762b4
mohammadreza kari <mohammadrezakari1234@gmail.com>
2026-01-28 09:24:22 +0330

    add domain layer sc-84465

--- internal/domain/todo/repository.go
+++ internal/domain/todo/repository.go
@@ -1,1 +1,7 @@
 package todo
+
+import "context"
+
+type Repository interface {
+	List(ctx context.Context) ([]*ToDo, error)
+}
--- internal/usecase/todo/todo.go
+++ internal/usecase/todo/todo.go
@@ -1,1 +1,18 @@
 package todo
+
+import (
+	domain "awesomeProject1/internal/domain/todo"
+	"context"
+)
+
+type ListUsers struct {
+	repo domain.Repository
+}
+
+func NewListUsers(repo domain.Repository) *ListUsers {
+	return &ListUsers{repo: repo}
+}
+
+func (uc *ListUsers) Execute(ctx context.Context) ([]*domain.ToDo, error) {
+	return uc.repo.List(ctx)
+}
