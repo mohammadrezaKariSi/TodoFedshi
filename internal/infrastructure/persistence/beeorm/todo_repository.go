f7ab86b7c4ac0909497993234b8842d77c50d4c0
mohammadreza kari <mohammadrezakari1234@gmail.com>
2026-02-02 09:11:20 +0330

    add script and redis consumer sc-84465

--- internal/infrastructure/persistence/beeorm/todo_repository.go
+++ internal/infrastructure/persistence/beeorm/todo_repository.go
@@ -9,7 +9,7 @@ import (
 )
 
 type ToDoEntity struct {
-	beeorm.ORM  `orm:"table=todos;dirty=todo_channel"`
+	beeorm.ORM  `orm:"table=todos"`
 	ID          uint64    `orm:"id"`
 	Description string    `orm:"description"`
 	DueDate     time.Time `orm:"due_date"`
